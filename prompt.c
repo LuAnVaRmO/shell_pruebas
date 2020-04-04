af689f22e046596e76e4d102d49ff29894cc08e2
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-04-04 10:43:57 -0500

    corrected exercise

--- prompt.c
+++ prompt.c
@@ -1,72 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-int start_prompt(char *buf, char *s);
-int _strcmp(char *s1, char *s2);
-
-char *buffer;
-char *s = "salir";
-size_t bufsize = 1;
-int characters;
-
+/**
+ * main Entry point
+ *
+ * Return: succes 0 | 1 if fails
+ */
 int main()
 {
-/**	pid_t my_pid;
-	my_pid = getpid();
-*/
-	buffer = (char *)malloc(bufsize * sizeof(char));
-	if( buffer == NULL)
+	char *buff;
+	size_t size = 1;
+	int received;
+
+	buff = (char *)malloc(size * sizeof(char));
+	if( buff == NULL)
 	{
 		perror("Unable to allocate buffer");
 		return (1);
 	}
+
 	printf("$ ");
-	characters = getline(&buffer,&bufsize,stdin);
-	if (characters < 0)
-		return (-1);
-	start_prompt(buffer, s);
-	free(buffer);
+	received = getline(&buff,&size,stdin);
+	if (received == -1)
+		return (1);
+	printf("%s",buff);
+	free(buff);
 	return(0);
-}
-int start_prompt(char *buffer,char *s)
-{
-	printf("%s, %s", buffer, s);
-
-	if (_strcmp(buffer,s) != 0)
-	{
-		printf("%s", buffer);
-		printf("$ ");
-		characters = getline(&buffer,&bufsize,stdin);
-		if (characters < 0)
-			return (-1);
-		start_prompt(buffer, s);
-	}
-	else
-	{
-		return(0);
-	}
-	return (0);
-}
-/**
- * _strcmp - compare two strings
- * @s1: string to compare
- * @s2: string to compare too
- * Return: int
- */
-int _strcmp(char *s1, char *s2)
-{
-	int i = 0;
-	int j;
-
-	while ((s1[i] != s2[i]) || (s1[i] != '\0'))
-	{
-		j = s1[i] - s2[i];
-		if (j != 0)
-		{
-			return (j);
-		}
-		i++;
-	}
-	return (0);
 }
