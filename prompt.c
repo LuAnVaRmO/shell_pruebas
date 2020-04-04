9dfb457581b459b1739bdf2858174362253d8ac0
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-04-04 10:49:22 -0500

    betty style

--- prompt.c
+++ prompt.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main Entry point
- *
+ * main - Entry point
+ * Description: Print text of the user with getline
  * Return: succes 0 | 1 if fails
  */
-int main()
+int main(void)
 {
 	char *buff;
 	size_t size = 1;
 	int received;
 
 	buff = (char *)malloc(size * sizeof(char));
-	if( buff == NULL)
+	if (buff == NULL)
 	{
 		perror("Unable to allocate buffer");
 		return (1);
 	}
 
 	printf("$ ");
-	received = getline(&buff,&size,stdin);
+	received = getline(&buff, &size, stdin);
 	if (received == -1)
 		return (1);
-	printf("%s",buff);
+	printf("%s", buff);
 	free(buff);
-	return(0);
+	return (0);
 }
